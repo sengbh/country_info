7893dac11a610f77d09114a3bff0135b2da46d75
sengbh <sengbh@ieee.org>
2019-04-21 22:00:38 -0500

    created new file

---
+++ lib/country_list.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+class HomeApp extends StatefulWidget {
+
+  @override
+  HomeAppState createState() {
+    return new HomeAppState();
+  }
+}
+
+class HomeAppState extends State<HomeApp> {
+  var countries;
+  Color mainColor = const Color(0xff3C3261);
+
+  void getData() async {
+    var data = await getJson();
+
+    setState(() {
+      countries = data ['results'];
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    getData();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: new AppBar(
+        elevation: 0.3,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: new Icon(
+          Icons.arrow_back,
+          color: mainColor,
+        ),
+        title: new Text(
+          'Movies',
+          style: new TextStyle(color: mainColor, fontFamily: 'Arvo', fontWeight: FontWeight.bold),
+        ),
+        actions: <Widget>[
+          new Icon(
+            Icons.menu,
+            color: mainColor,
+          ),
+        ],
+      ),
+      body: new Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new CountryTitle(mainColor),
+            new Expanded(
+              child: new ListView.builder(
+                itemCount: countries == null ? 0 : countries.length,
+                itemBuilder: (context, i) {
+                  return new FlatButton(
+                    child: new CountryCell(countries, i),
+                    padding: const EdgeInsets.all(0.0),
+                    color:  Colors.white,
+                  );
+                }
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+class CountryCell extends StatelessWidget {
+  final countries, i;
+
+  Color mainColor = const Color(0xff3C3261);
+  var image_url = 'https://image.tmdb.org/t/p/w500/';
+  CountryCell(this.countries, this.i);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Row(
+          children: <Widget>[
+            new Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: new Container(
+                margin: const EdgeInsets.all(16.0),
+                child: new Container(
+                  width: 70.0,
+                  height: 70.0,
+                ),
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.circular(10.0),
+                  color: Colors.grey,
+                  image: new DecorationImage(
+                    image: new NetworkImage(
+                      image_url + countries[i]['poster_path']
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: mainColor,
+                      blurRadius: 5.0,
+                      offset: new Offset(2.0, 5.0)
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Expanded(
+              child: new Container(
+                margin: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+                child: new Column(
+                  children: [
+                    new Text(
+                      countries[i]['title'],
+                      style: new TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Arvo',
+                        fontWeight: FontWeight.bold,
+                        color: mainColor,
+                      ),
+                    ),
+                    new Padding(
+                      padding: const EdgeInsets.all(2.0)
+                    ),
+                    new Text(
+                      countries[i]['overview'],
+                      maxLines: 3,
+                      style: new TextStyle(
+                        color: const Color(0xff8785A4),
+                        fontFamily: 'Arvo'
+                      ),
+                    ),
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              ),
+            )
+          ],
+        ),
+        new Container(
+          width: 300.0,
+          height: 0.5,
+          color: const Color(0xD2D2E1ff),
+          margin: const EdgeInsets.all(16.0),
+        )
+      ],
+    );
+  }
+}
+
+class CountryTitle extends StatelessWidget {
+  final Color mainColor;
+
+  CountryTitle(this.mainColor);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      child: new Text(
+        'Top Rated',
+        style: new TextStyle(
+          fontSize:  40.0,
+          color: mainColor,
+          fontWeight:  FontWeight.bold,
+          fontFamily: 'Arvo', 
+          ),
+          textAlign: TextAlign.left,
+      ),
+    );
+  }
+}
+
+Future<Map> getJson() async {
+  var url = '';
+  http.Response response = await http.get(url);
+  return json.decode(response.body);
+}
