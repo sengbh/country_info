69b227e16f5124734da7cfac82831083d88463fe
sengbh <sengbh@ieee.org>
2019-04-21 21:59:49 -0500

    forwarded to new file via import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'country_list.dart';
 
 void main() => runApp(new MainApp());
 
@@ -8,132 +9,133 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //title: 'Country List',
-      home: HomeApp(),
+      home: new HomeApp(),
     );
   }
 }
 
-class HomeApp extends StatefulWidget {
-  State<StatefulWidget> createState() {
-    return _HomeState();
-  }
-}
 
-class _HomeState extends State<HomeApp>{
-  int _currentIndex = 0;
-  final List<Widget> _children = [
-    ColorTab(Colors.white),
-    ColorTab(Colors.grey),
-    ColorTab(Colors.brown),
-  ];
+// class HomeApp extends StatefulWidget {
+//   State<StatefulWidget> createState() {
+//     return _HomeState();
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: TapBarTitle(),
-      ),
-      body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabT,
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            title: new Text("Home"),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.account_circle),
-            title: new Text("STUFF"),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.person),
-            title: new Text("Profile"),
-          ),
-        ],
-      ),
-    );
-  }
+// class _HomeState extends State<HomeApp>{
+//   int _currentIndex = 0;
+//   final List<Widget> _children = [
+//     ColorTab(Colors.white),
+//     ColorTab(Colors.grey),
+//     ColorTab(Colors.brown),
+//   ];
 
-  void onTabT(int index) {
-    setState((){
-      _currentIndex = index;
-    });
-  } 
-}
-
-class ColorTab extends StatelessWidget {
-  final Color color;
-  ColorTab(this.color);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: new Center(
-        child: RaisedButton(
-          child: new Text("click me!"),
-          onPressed: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => PageTwo())
-            );
-          },
-        ),
-      ),  
-    );
-  }
-}
-
-// class PageOne extends StatelessWidget{
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       appBar: AppBar(
 //         centerTitle: true,
-//         title: new Text("Home Screen")
+//         title: TapBarTitle(),
 //       ),
-//       body: Center(
-//         child: RaisedButton(
-//           child: new Text("Open page"),
-//           onPressed: (){
-//             Navigator.push(
-//               context,
-//               MaterialPageRoute(builder: (context) => PageTwo()) 
-//             );
-//           },
-//         ),
+//       body: _children[_currentIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         onTap: onTabT,
+//         currentIndex: _currentIndex,
+//         items: [
+//           BottomNavigationBarItem(
+//             icon: new Icon(Icons.home),
+//             title: new Text("Home"),
+//           ),
+//           BottomNavigationBarItem(
+//             icon: new Icon(Icons.account_circle),
+//             title: new Text("STUFF"),
+//           ),
+//           BottomNavigationBarItem(
+//             icon: new Icon(Icons.person),
+//             title: new Text("Profile"),
+//           ),
+//         ],
 //       ),
 //     );
 //   }
 
+//   void onTabT(int index) {
+//     setState((){
+//       _currentIndex = index;
+//     });
+//   } 
 // }
 
-class PageTwo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: new Center(
-          child: new Text("Details"),
-        ),
-      ),
-      body: new Center(
-        child: new Text("123 back!", style: TextStyle(fontSize: 24.0),),
-      ),
-    );   
-  } 
-  
-}
+// class ColorTab extends StatelessWidget {
+//   final Color color;
+//   ColorTab(this.color);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: new Center(
+//         child: RaisedButton(
+//           child: new Text("click me!"),
+//           onPressed: (){
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => PageTwo())
+//             );
+//           },
+//         ),
+//       ),  
+//     );
+//   }
+// }
 
-class TapBarTitle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: new Text("Home Screen"),
-      ),
-    );
+// // class PageOne extends StatelessWidget{
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         centerTitle: true,
+// //         title: new Text("Home Screen")
+// //       ),
+// //       body: Center(
+// //         child: RaisedButton(
+// //           child: new Text("Open page"),
+// //           onPressed: (){
+// //             Navigator.push(
+// //               context,
+// //               MaterialPageRoute(builder: (context) => PageTwo()) 
+// //             );
+// //           },
+// //         ),
+// //       ),
+// //     );
+// //   }
+
+// // }
+
+// class PageTwo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: new Center(
+//           child: new Text("Details"),
+//         ),
+//       ),
+//       body: new Center(
+//         child: new Text("123 back!", style: TextStyle(fontSize: 24.0),),
+//       ),
+//     );   
+//   } 
+  
+// }
+
+// class TapBarTitle extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: new Text("Home Screen"),
+//       ),
+//     );
     
-  }
-}
+//   }
+// }
