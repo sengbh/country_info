e2706c4030c151e63575c8a037a3753307cae5fe
sengbh <sengbh@ieee.org>
2019-04-22 14:38:32 -0500

    added country details file

---
+++ lib/country_details.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+class CountryDetails extends StatelessWidget {
+  final country;
+  var imageUrl = 'http://66.media.tumblr.com/8db0d541e8bd5b7d2b06b5144b764498/tumblr_np297trsYs1skelofo1_500.jpg';
+  CountryDetails(this.country);
+  Color mainColor = const Color(0xff3C3261);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(fit: StackFit.expand, children: [
+        new Image.network(
+          imageUrl + country['poster_path'],
+          fit: BoxFit.cover,
+        ),
+        new BackdropFilter(
+          filter: new ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+          child: new Container(
+            color: Colors.black.withOpacity(0.5),
+          ),
+        ),
+        new SingleChildScrollView(
+          child: new Container(
+            margin: const EdgeInsets.all(20.0),
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  alignment: Alignment.center,
+                  child: new Container(
+                    width: 400.0,
+                    height: 400.0,
+                  ),
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(10.0),
+                      image: new DecorationImage(
+                          image: new NetworkImage(
+                              imageUrl + country['poster_path']),
+                          fit: BoxFit.cover),
+                      boxShadow: [
+                        new BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 20.0,
+                            offset: new Offset(0.0, 10.0))
+                      ]),
+                ),
+                new Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 20.0, horizontal: 0.0),
+                  child: new Row(
+                    children: <Widget>[
+                      new Expanded(
+                          child: new Text(
+                        country['title'],
+                        style: new TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                            fontFamily: 'Arvo'),
+                      )),
+                      new Text(
+                        '${country['vote_average']}/10',
+                        style: new TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontFamily: 'Arvo'),
+                      )
+                    ],
+                  ),
+                ),
+                new Text(country['overview'],style: new TextStyle(color: Colors.white, fontFamily: 'Arvo')),
+                new Padding(padding: const EdgeInsets.all(10.0)),
+                new Row(
+                  children: <Widget>[
+                    new Expanded(
+                        child: new Container(
+                      width: 150.0,
+                      height: 60.0,
+                      alignment: Alignment.center,
+                      child: new Text(
+                        'Rate Country',
+                        style: new TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Arvo',
+                            fontSize: 20.0),
+                      ),
+                      decoration: new BoxDecoration(
+                          borderRadius: new BorderRadius.circular(10.0),
+                          color: const Color(0xaa3C3261)),
+                    )),
+                    new Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: new Container(
+                        padding: const EdgeInsets.all(16.0),
+                        alignment: Alignment.center,
+                        child: new Icon(
+                          Icons.share,
+                          color: Colors.white,
+                        ),
+                        decoration: new BoxDecoration(
+                            borderRadius: new BorderRadius.circular(10.0),
+                            color: const Color(0xaa3C3261)),
+                      ),
+                    ),
+                    new Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: new Container(
+                          padding: const EdgeInsets.all(16.0),
+                          alignment: Alignment.center,
+                          child: new Icon(
+                            Icons.bookmark,
+                            color: Colors.white,
+                          ),
+                          decoration: new BoxDecoration(
+                              borderRadius: new BorderRadius.circular(10.0),
+                              color: const Color(0xaa3C3261)),
+                        )
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
